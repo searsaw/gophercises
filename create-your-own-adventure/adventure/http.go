8ef1015560e80169548476b86700c102da059fca
Alex Sears <me@alexsears.com>
2018-05-06 17:32:51 -0400

    Finished 'choose your own adventure'

--- create-your-own-adventure/adventure/http.go
+++ create-your-own-adventure/adventure/http.go
@@ -1,12 +1,26 @@
 package adventure
 
 import (
+	"html/template"
 	"net/http"
 )
 
-func NewHttpAdventure(jsonData []byte) (*http.ServeMux, error) {
+func NewHttpAdventure(jsonData []byte, templateFile string) (*http.ServeMux, error) {
 	story, err := parseJSONData(jsonData)
 	if err != nil {
 		return nil, err
 	}
+
+	router := http.NewServeMux()
+	tmpl := template.Must(template.ParseFiles(templateFile))
+
+	for slug, storyArc := range story {
+		func(slug string, storyArc StoryArc) {
+			router.HandleFunc("/"+slug, func(w http.ResponseWriter, r *http.Request) {
+				tmpl.Execute(w, storyArc)
+			})
+		}(slug, storyArc)
+	}
+
+	return router, nil
 }
--- create-your-own-adventure/main.go
+++ create-your-own-adventure/main.go
@@ -2,22 +2,42 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+
+	"github.com/searsaw/gophercises/create-your-own-adventure/adventure"
 )
 
 func createRouter() *http.ServeMux {
 	router := http.NewServeMux()
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/intro", http.StatusTemporaryRedirect)
-	})
+	// router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.Redirect(w, r, "/intro", http.StatusTemporaryRedirect)
+	// })
 	return router
 }
 
 func main() {
-	adventureFilename := flag.String("filename", "adventure.json", "a JSON file containing the adventure story data")
+	adventureFilename := flag.String("adventure", "adventure.json", "a JSON file containing the adventure story data")
+	templateFilename := flag.String("template", "template.html", "a Golang template file used to show each page")
+	port := flag.Int("port", 8000, "port the server runs on")
 	flag.Parse()
 
-	router := createRouter()
+	fileData, err := ioutil.ReadFile(*adventureFilename)
+	if err != nil {
+		fmt.Printf("There was a problem when opening the story file: %s\n", err.Error())
+		os.Exit(1)
+	}
 
-	http.ListenAndServe(":8000", router)
+	router := createRouter()
+	adventureMux, err := adventure.NewHttpAdventure(fileData, *templateFilename)
+	if err != nil {
+		fmt.Printf("There was a problem when setting up the adventure mux: %s\n", err.Error())
+		os.Exit(1)
+	}
+	router.Handle("/", adventureMux)
+
+	fmt.Printf("The server is now running on port %d.\n", *port)
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), router)
 }
